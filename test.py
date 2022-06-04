6d23f0025494f4d957c97a76dac75c6a0b8c80aa
DErkan-A <80825222+DErkan-A@users.noreply.github.com>
2022-06-04 18:47:48 +0300

    Updated to new version provided

--- test.py
+++ test.py
@@ -1,44 +1,47 @@
-import time
+import os
+import sys
+import time, random, math
 from enum import Enum
-from adhoccomputing import GenericModel, Event, Generics, Definitions, Topology, FramerObjects, FrameHandlerBase, ofdm_callback, MacCsmaPPersistentConfigurationParameters, MacCsmaPPersistent, UsrpB210OfdmFlexFramePhy
-from ctypes import *
+from pickle import FALSE
+
+
+
+from adhoccomputing.GenericModel import GenericModel
+from adhoccomputing.Generics import *
+from adhoccomputing.Experimentation.Topology import Topology
+from adhoccomputing.Networking.PhysicalLayer.UsrpB210OfdmFlexFramePhy import  UsrpB210OfdmFlexFramePhy
+from adhoccomputing.Networking.MacProtocol.CSMA import MacCsmaPPersistent, MacCsmaPPersistentConfigurationParameters
 
 
 # define your own message types
 class ApplicationLayerMessageTypes(Enum):
     BROADCAST = "BROADCAST"
 
+
 # define your own message header structure
-class ApplicationLayerMessageHeader(Generics.GenericMessageHeader):
+class ApplicationLayerMessageHeader(GenericMessageHeader):
     pass
 
-# define your own EventTypes 
+
 class UsrpApplicationLayerEventTypes(Enum):
     STARTBROADCAST = "startbroadcast"
 
 
-# In this part, by using Generic Model provided from adhoccomputing library, an USRP application layer is generated and extended. 
-# The main idea behind the adhoccomputing library is meeting the only basic needs for AdHoc Computing. 
-# The Code that provided can be extended to cover any kind of protocols, layers, algorithms and etc. 
-# In this USRPApplicationLayer, people extended the GenericModel and override the methods:
-#  -- __init__ -- on_message_from_top -- on_message_from_bottom
-# In addition to that, also new method is added which is on_startbroadcast as you can see below.
 class UsrpApplicationLayer(GenericModel):
-    # def on_init(self, eventobj: Event):
-
-    def __init__(self, componentname, componentid):
-        super().__init__(componentname, componentid)
+    def on_init(self, eventobj: Event):
         self.counter = 0
-        # Event handler uses Eventypes to Call Proper Events from other layers or trigger events 
+    
+    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None):
+        super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
         self.eventhandlers[UsrpApplicationLayerEventTypes.STARTBROADCAST] = self.on_startbroadcast
 
     def on_message_from_top(self, eventobj: Event):
     # print(f"I am {self.componentname}.{self.componentinstancenumber},sending down eventcontent={eventobj.eventcontent}\n")
-        self.send_down(Event(self, Definitions.EventTypes.MFRT, eventobj.eventcontent))
-
+        self.send_down(Event(self, EventTypes.MFRT, eventobj.eventcontent))
+    
     def on_message_from_bottom(self, eventobj: Event):
-        evt = Event(self, Definitions.EventTypes.MFRT, eventobj.eventcontent)
-        print(f"I am Node.{self.componentinstancenumber}, received from Node.{eventobj.eventcontent.header.messagefrom} a message: {eventobj.eventcontent.payload}")
+        evt = Event(self, EventTypes.MFRT, eventobj.eventcontent)
+        logger.applog(f"I am Node.{self.componentinstancenumber}, received from Node.{eventobj.eventcontent.header.messagefrom} a message: {eventobj.eventcontent.header.sequencenumber}")    
         if self.componentinstancenumber == 1:
             evt.eventcontent.header.messageto = 0
             evt.eventcontent.header.messagefrom = 1
@@ -46,81 +49,87 @@ class UsrpApplicationLayer(GenericModel):
             evt.eventcontent.header.messageto = 1
             evt.eventcontent.header.messagefrom = 0
         evt.eventcontent.payload = eventobj.eventcontent.payload
+        evt.eventcontent.header.sequencenumber = eventobj.eventcontent.header.sequencenumber + 1
         #print(f"I am {self.componentname}.{self.componentinstancenumber}, sending down eventcontent={eventobj.eventcontent.payload}\n")
         self.send_down(evt)  # PINGPONG
-
+    
     def on_startbroadcast(self, eventobj: Event):
         if self.componentinstancenumber == 1:
             hdr = ApplicationLayerMessageHeader(ApplicationLayerMessageTypes.BROADCAST, 1, 0)
         else:
             hdr = ApplicationLayerMessageHeader(ApplicationLayerMessageTypes.BROADCAST, 0, 1)
         self.counter = self.counter + 1
-
-        payload = "BMSG-" + str(self.counter)
-        broadcastmessage = Generics.GenericMessage(hdr, payload)
-        evt = Event(self, Definitions.EventTypes.MFRT, broadcastmessage)
+        hdr.sequencenumber = 1
+        payload = "BMSG-"*200 + str(self.counter)
+        broadcastmessage = GenericMessage(hdr, payload)
+        evt = Event(self, EventTypes.MFRT, broadcastmessage)
         # time.sleep(3)
         self.send_down(evt)
-
-
-# The USRPNode is like a AdHocNode that is provided by adhoccomputing library. It behaves like a 
-# container for layers. In this node, we have application - physical and mac components. 
-# As you can see we have Connector Types - UP - DOWN and more. These components creates an order between 
-# components so that the messaging between components will be handled by send_down -- trigger_event and more.
+        #print("Starting broadcast")
+    
+         
 class UsrpNode(GenericModel):
     counter = 0
     def on_init(self, eventobj: Event):
         pass
-
-    def __init__(self, componentname, componentid):
+    
+    def __init__(self, componentname, componentinstancenumber, context=None, configurationparameters=None, num_worker_threads=1, topology=None):
+        super().__init__(componentname, componentinstancenumber, context, configurationparameters, num_worker_threads, topology)
         # SUBCOMPONENTS
-
-        macconfig = MacCsmaPPersistentConfigurationParameters(0.5)
-
-        self.appl = UsrpApplicationLayer("UsrpApplicationLayer", componentid)
-        self.phy = UsrpB210OfdmFlexFramePhy("UsrpB210OfdmFlexFramePhy", componentid)
-        self.mac = MacCsmaPPersistent("MacCsmaPPersistent", componentid,  configurationparameters=macconfig, uhd=self.phy.ahcuhd)
+        
+        macconfig = MacCsmaPPersistentConfigurationParameters(0.5, -51)
+        #usrpconfig = SDRConfiguration(freq =900000000.0, bandwidth = 250000, chan = 0, hw_tx_gain = 50.0, hw_rx_gain = 20.0, sw_tx_gain = -12.0)
+        #sdrconfig = SDRConfiguration(freq =915000000.0, bandwidth = 2000000, chan = 0, hw_tx_gain = 70, hw_rx_gain = 70, sw_tx_gain = -12.0)
+        #sdrconfig = SDRConfiguration(freq =915000000.0, bandwidth = 20000000, chan = 0, hw_tx_gain = 76, hw_rx_gain = 20, sw_tx_gain = -12.0)
+        sdrconfig = SDRConfiguration(freq =2484000000.0, bandwidth = 20000000, chan = 0, hw_tx_gain = 76, hw_rx_gain = 20, sw_tx_gain = -12.0)
+        bladerfconfig = SDRConfiguration(freq =900000000.0, bandwidth = 250000, chan = 0, hw_tx_gain = 50.0, hw_rx_gain = 20.0, sw_tx_gain = -12.0)
+        
+        self.appl = UsrpApplicationLayer("UsrpApplicationLayer", componentinstancenumber, topology=topology)
+        self.phy = UsrpB210OfdmFlexFramePhy("UsrpB210OfdmFlexFramePhy", componentinstancenumber, usrpconfig=sdrconfig, topology=topology)
+        print(self.phy.sdrdev)
+        self.mac = MacCsmaPPersistent("MacCsmaPPersistent", componentinstancenumber,  configurationparameters=macconfig, sdr=self.phy.sdrdev,topology=topology)
+        
+        self.components.append(self.appl)
+        self.components.append(self.phy)
+        self.components.append(self.mac)
 
         # CONNECTIONS AMONG SUBCOMPONENTS
-        self.appl.connect_me_to_component(Definitions.ConnectorTypes.UP, self) #Not required if nodemodel will do nothing
-        self.appl.connect_me_to_component(Definitions.ConnectorTypes.DOWN, self.mac)
-
-        self.mac.connect_me_to_component(Definitions.ConnectorTypes.UP, self.appl)
-        self.mac.connect_me_to_component(Definitions.ConnectorTypes.DOWN, self.phy)
-
+        self.appl.connect_me_to_component(ConnectorTypes.UP, self) #Not required if nodemodel will do nothing
+        self.appl.connect_me_to_component(ConnectorTypes.DOWN, self.mac)
+        
+        self.mac.connect_me_to_component(ConnectorTypes.UP, self.appl)
+        self.mac.connect_me_to_component(ConnectorTypes.DOWN, self.phy)
+        
         # Connect the bottom component to the composite component....
-        self.phy.connect_me_to_component(Definitions.ConnectorTypes.UP, self.mac)
-        self.phy.connect_me_to_component(Definitions.ConnectorTypes.DOWN, self)
-
+        self.phy.connect_me_to_component(ConnectorTypes.UP, self.mac)
+        self.phy.connect_me_to_component(ConnectorTypes.DOWN, self)
+        
         # self.phy.connect_me_to_component(ConnectorTypes.DOWN, self)
         # self.connect_me_to_component(ConnectorTypes.DOWN, self.appl)
-
-        super().__init__(componentname, componentid)
-
-
-
+    
+        
 
 def main():
+    setAHCLogLevel(LOG_LEVEL_APPLOG)
     topo = Topology()
-    # Note that the Topology has to specific: usrp winslab_b210_0 is run by instance 0 of the component
-    # Therefore, the usrps have to have names winslab_b210_x where x \in (0 to nodecount-1)
-    # In this topology, we dont have channels between nodes. We handle that part by broadcasting messages 
-    # by using USRP devices. However, you can create topologies with custom channels too by using adhoccomputing
-    # library. Unfortunatelly, it is not currently compatible with USRPdevices but in upcomping weeks 
-    # we will bring that feature too. 
-    topo.construct_winslab_topology_without_channels(4, UsrpNode)
+# Note that the topology has to specific: usrp winslab_b210_0 is run by instance 0 of the component
+# Therefore, the usrps have to have names winslab_b210_x where x \in (0 to nodecount-1)
+    topo.construct_winslab_topology_without_channels(2, UsrpNode)
+  # topo.construct_winslab_topology_with_channels(2, UsrpNode, FIFOBroadcastPerfectChannel)
+  
+  # time.sleep(1)
+  # topo.nodes[0].send_self(Event(topo.nodes[0], UsrpNodeEventTypes.STARTBROADCAST, None))
 
-    # start funtion in topology generates a forwarding table for network layer operations. However, in this case it 
-    # may be unneccesary.
     topo.start()
     i = 0
-    while(i < 10):
-        topo.nodes[1].appl.send_self(Event(topo.nodes[0], UsrpApplicationLayerEventTypes.STARTBROADCAST, None))
-        time.sleep(1)
+    while(i < 100):
+        #topo.nodes[1].appl.send_self(Event(topo.nodes[1], UsrpApplicationLayerEventTypes.STARTBROADCAST, None))
+        #time.sleep(0.1)
+        topo.nodes[0].appl.send_self(Event(topo.nodes[0], UsrpApplicationLayerEventTypes.STARTBROADCAST, None))
+        time.sleep(0.1)
         i = i + 1
+    topo.exit()
 
 
 if __name__ == "__main__":
     main()
-
-
